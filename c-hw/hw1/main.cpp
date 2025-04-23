dce940c6fc90f1e6ac40bec6c3d04b24e2f97731
helen <helenceliik@gmail.com>
2025-04-23 13:37:26 +0300

    c hw1 is refactored

--- c-hw/hw1/main.cpp
+++ c-hw/hw1/main.cpp
@@ -1,37 +1,62 @@
 #include <stdio.h>
-struct ogrenci{
+
+struct ogrenci
+{
 	char isim[50];
 	int numara;
 	int yoklama;
 };
-int main(){
+
+int main()
+{
 	int sayi, i;
+
 	printf("kac ogrenci var?:");
 	scanf("%d", &sayi);
+
 	struct ogrenci ogrenciler[sayi];
-	for(i=0;i<sayi;i++){
+
+	for(i=0; i < sayi; i++)
+	{
 		printf("%d. ogrencinin bilgilerini giriniz(isim, numara):",i+1);
 		scanf("%s %d",ogrenciler[i].isim, &ogrenciler[i].numara);
 	}
-	for(i=0;i<sayi;i++){
+
+	for(i=0; i < sayi; i++)
+	{
 		char cevap;
+
 		printf("%s sinifta mi? (+/-):", ogrenciler[i].isim);
 		scanf(" %c",&cevap);
-		if(cevap == '+'){
+
+		if(cevap == '+')
+		{
 			ogrenciler[i].yoklama= 1;
-		}else if(cevap == '-'){
+		}
+		else if(cevap == '-')
+		{
 			ogrenciler[i].yoklama= 0;
-		}else{
+		}
+		else
+		{
 			printf("hatali giris yaptiniz.");
 			i--;
 		}
 	}
+
 	printf("\n---YOKLAMA---\n");
-	for(i=0;i<sayi;i++){
-		if (ogrenciler[i].yoklama== 1){
-		printf("%s sinifta.\n", ogrenciler[i].isim);}
-		else {
-		printf("%s sinifta degil.",ogrenciler[i].isim);}
+
+	for(i=0; i < sayi; i++)
+	{
+		if (ogrenciler[i].yoklama== 1)
+		{
+			printf("%s sinifta.\n", ogrenciler[i].isim);
+		}
+		else
+		{
+		printf("%s sinifta degil.",ogrenciler[i].isim)
+		}
 	}
+
 	return 0;
 }
